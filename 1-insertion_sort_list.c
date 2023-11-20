fc3802436cfab1c8d7171599884ea3c5bfb8201c
wishon1 <wisdomhonest1@gmail.com>
2023-11-20 15:42:24 -0500

    add insertion of double link list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,35 +1,39 @@
-#include "sort.h"
-
+#include"sort.h"
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending
- * @list: a listint_t head poiter
- * Return: void
+ * insertion_sort_list - sorts a doubly linked list of
+ * integers in ascending order
+ * @list: is a pointer to the list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *right = *list;
-	listint_t *left;
-	listint_t *insert;
-	int current_n;
+	listint_t *temp, *pre_node = NULL, *current;
+	int key;
 
-	right = right->next;
-	while (right != NULL)
+	if (*list == NULL || (*list)->next == NULL)
+		return;
+	current = (*list);
+
+	while (current != NULL)
 	{
-		current_n = right->n;
-		left = right->prev;
-		while (left != NULL && current_n < left->n)
+		temp = current;
+		pre_node = temp->prev;
+		key = temp->n;
+		current = current->next;
+		while (pre_node != NULL && pre_node->n > key)
 		{
-			left = left->prev;
+			pre_node->next = temp->next;
+			if (temp->next)
+				temp->next->prev = pre_node;
+
+			temp->prev = pre_node->prev;
+			if (pre_node->prev)
+				pre_node->prev->next = temp;
+			pre_node->prev = temp;
+			temp->next = pre_node;
+			pre_node = temp->prev;
+			if ((*list)->prev != NULL)
+				*list = temp;
+			print_list(*list);
 		}
-		insert = right;
-		insert->prev->next = insert->next;
-		insert->next->prev = insert->prev;
-
-		insert->prev = left;
-		insert->next = left->next;
-
-		left->next = insert;
-		left->next->prev = insert;
-		right = right->next;
 	}
 }
